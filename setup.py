5505366b2f7c263ce0535590165def875cde72ea
biloucat <ferry.pierre@orange.fr>
2017-04-16 19:26:52 +0200

    version 0.9 add internationalization

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 setup(
     name="tkencfs",
-    version="0.8",
+    version="0.9",
     description="EncFS GUI for encrypted directory mounting",
     url="https://github.com/biloucat/tkencfs",
     license="GPL-3.0",
     platforms="Archlinux",
     #py_modules=['tkencfs'],
     scripts=['tkencfs'],
-    data_files=[('share/applications',['tkencfs.desktop']),('share/pixmaps',['tkencfs.png'])]
+    data_files=[('share/applications',['tkencfs.desktop']),
+                ('share/pixmaps',['tkencfs.png']),
+                ('/usr/share/locale/fr/LC_MESSAGES',['i18n/fr/LC_MESSAGES/tkencfs.mo'])]
 )
